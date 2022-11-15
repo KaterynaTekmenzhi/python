f461e6d50b671fceca65647f2480db979297c634
Kateryna Tekmenzhi <kateryna.koltunova@gmail.com>
2022-11-15 13:29:46 -0800

    basic syntax

--- script.py
+++ script.py
@@ -2,3 +2,15 @@
 
 #1. Using a "print" statement, output a message of your choosing to the terminal.
 print "Hello Kat!"
+#2. Python 3 has a parenthesis around the print statement. Python 2 does not. 
+print ("Hello Kat!")
+#3. Strings can be numbers letters, or symbols.
+print 'Hello Kat!'
+#4. connect strings together using the + sign.
+print "hi" + "my name is kat"
+#5. (Handling errors) mismatched quotes will cause a syntac error. No quotes will cause a name error.
+print ("what did you eat for dinner?")
+print ('i had salmon')
+#6. (Variables)
+greeting_message = "welcome kat!"
+print greeting_message
